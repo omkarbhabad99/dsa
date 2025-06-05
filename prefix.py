7688d568bd70f9d4be6495440d47cda907e34563
omkarbhabad99 <82103261+omkarbhabad99@users.noreply.github.com>
2025-06-05 13:53:08 -0400

    DSA Advanced prefix and subarrays

--- prefix.py
+++ prefix.py
@@ -23,3 +23,30 @@ class Solution:
             ans[i] = ans[i-1] + pf_sum[i]
         
         return ans
+
+
+
+
+
+# Problem Description
+
+# Kamal is a software developer and he's working on a new feature for an e-commerce website. The website has a list of prices for different products, and Kamal needs to find the maximum price of all products up to a given index.
+
+# He has the list of prices in an array A of length N, and he needs to write a program that will return the maximum price occurring in the subarray from 0 to i for every index i. Kamal needs your help to implement this function.
+
+
+class Solution:
+    # @param A : list of integers
+    # @return a list of integers
+    def solve(self, A):
+        ans = []
+        ans.append(A[0])
+
+        for i in range(1,len(A)):
+
+            if A[i] > ans[i-1]:
+                ans.append(A[i])
+            else:
+                ans.append(ans[i-1])
+
+        return ans
